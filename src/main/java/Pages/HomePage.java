e1dfd39667c37299abc02f799ff9f66dd7528437
Bruno Bispo <bruno.bispo@solutis.com.br>
2019-11-13 12:23:27 -0300

    Ajuste da Classe UsuarioTest

--- src/main/java/Pages/HomePage.java
+++ src/main/java/Pages/HomePage.java
@@ -1,8 +1,9 @@
 package Pages;
 
 import Helper.BaseTest;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.*;
+
+import java.util.concurrent.TimeUnit;
 
 public class HomePage extends BaseTest {
 
@@ -16,7 +17,7 @@ public class HomePage extends BaseTest {
     public void acessarMenuUsuario() {
 
         driver.findElement(By.cssSelector(".MuiButton-label > #btn-consumidor")).click();
-        driver.findElement(By.cssSelector("div.jss338:nth-child(1)")).click();
+        driver.findElement(By.cssSelector("#root > div > div > div._loading_overlay_wrapper.css-79elbk > main > div.MuiGrid-root.jss161.MuiGrid-container.MuiGrid-direction-xs-column.MuiGrid-align-items-xs-center.MuiGrid-justify-xs-center > div.MuiGrid-root.MuiGrid-container.MuiGrid-align-items-xs-center.MuiGrid-justify-xs-center > div:nth-child(1) > div > p")).click();//refatorar esse selector
     }
 
     public void acessarMenuEmpresa(){
@@ -24,13 +25,39 @@ public class HomePage extends BaseTest {
 
     }
 
-    public void preencherDadosUsuario(String nome, String cpf)  {
+    public void preencherDadosUsuario(String nome, String cpf) throws InterruptedException {
 
         driver.findElement(By.id("cpf")).sendKeys(cpf);
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+        Thread.sleep(2000);//refatorar esse trecho-- retirar o sleep e ajustar o trecho do timeout
+        WebElement webElement = driver.findElement(By.id("cpf"));
+        webElement.sendKeys(Keys.TAB);
+
         driver.findElement(By.id("senha")).sendKeys("123456");
         driver.findElement(By.id("nomeCompleto")).sendKeys(nome);
+        driver.findElement(By.id("email")).sendKeys(("teste@automatizado.com"));
         driver.findElement(By.id("telefone")).sendKeys("2127272727");
+        Thread.sleep(2000);//refatorar esse trecho-- retirar o sleep e ajustar o trecho do timeout
+        driver.findElement(By.xpath(("//*[@id=\"root\"]/div/div/div[1]/main/div[2]/div/form/div[2]/div[2]/div/div/div/button"))).click();
+
+        //marcações checkbox
+        driver.findElement(By.id("titularTel")).click();
+        driver.findElement(By.id("maxTel")).click();
+        driver.findElement(By.id("prazoRec")).click();
+
+        driver.findElement(By.id(("btnSalvar"))).click();
 
    }
+
+   public void preencherDadosEmpresa(String cnpj){
+
+       driver.findElement(By.name("cnpj")).sendKeys(cnpj);
+       driver.findElement(By.name("senha")).sendKeys("123456");
+       driver.findElement(By.name("cpfResponsavel")).sendKeys("123456");
+
+
+
+   }
+
 
 }
--- src/main/java/Tests/UsuarioTest.java
+++ src/main/java/Tests/UsuarioTest.java
@@ -3,10 +3,12 @@ package Tests;
 import Helper.BaseTest;
 import Pages.HomePage;
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 import java.util.concurrent.TimeUnit;
 
@@ -19,30 +21,33 @@ public class UsuarioTest extends BaseTest {
     public void setUp() throws Exception {
 
         System.setProperty("webdriver.chrome.driver", "C:/chromedriver.exe");
-        driver = new ChromeDriver();
+        ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.addArguments("--headless");
+        driver = new ChromeDriver(chromeOptions);
         driver.get("https://procon-carioca-web.clv.iplan.dev.solutis.xyz/");
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-        //HomePage hometest = new HomePage(driver);
+
 
     }
 
 
     @Test
-    public void cadastrarUsuarioTest()  {
+    public void cadastrarUsuarioTest() throws InterruptedException {
 
         HomePage homepage = new HomePage(driver);
 
         homepage.acessarMenuUsuario();
-        homepage.preencherDadosUsuario("14075299783", "TESTE");
-
+        homepage.preencherDadosUsuario("NOME COMPLETO", "28606850063");
+        String URL = driver.getCurrentUrl();
+        Assert.assertEquals(URL,"https://procon-carioca-web.clv.iplan.dev.solutis.xyz/consumidor");
     }
 
 
     @After
     public void tearDown() throws Exception {
 
-        driver.close();
+       // driver.quit();
 
     }
 
